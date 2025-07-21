3c054f938a00166e1c0845d08d260e9494483969
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2025-07-20 21:18:15 -0600

    Initial Commit

---
+++ src/main/java/com/lib/servelet/PruebaServ.java
@@ -0,0 +1,27 @@
+package com.lib.servelet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("prueba")
+public class PruebaServ extends HttpServlet {
+
+    public PruebaServ() {
+        super();
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String nombre = req.getParameter("nombre");
+        String apellido = req.getParameter("apellido");
+        res.getWriter().append("Servet Servlet: ").append("Nombre: ").append(nombre).append(" Apellido: ").append(apellido);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+}
